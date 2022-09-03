398541d5b927cf066a38176b3f51458743b2a324
Serhii Bulychov <woyager69@gmail.com>
2022-09-04 01:25:55 +0300

    add video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const currentTime = localStorage.getItem('playing-current-time');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const onTimeupdate = seconds => {
+  localStorage.setItem('playing-current-time', seconds);
+};
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    onTimeupdate(seconds);
+  }, 1000)
+);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
